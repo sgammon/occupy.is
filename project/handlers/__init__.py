000de7f61867a6ac3cf0a54f55cb881a247a2d72
Sam Gammon <sgammon@bluestatedigital.com>
2011-12-09 15:25:07 -0800

    Initial commit.

---
+++ project/handlers/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+## AppTools Imports
+from apptools.core import BaseHandler
+import logging
+
+class WebHandler(BaseHandler):
+	
+	''' Handler for desktop web requests. '''
+	
+class MobileHandler(BaseHandler):
+	
+	''' Handler for mobile web requests. '''
