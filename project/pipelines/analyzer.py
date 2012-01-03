bce29dde021675fb121799fe09926383605c21d0
Tyler Porras <ty.porras@gmail.com>
2012-01-02 17:42:03 -0800

    Built pipeline shells for analyzer.py

--- project/pipelines/analyzer.py
+++ project/pipelines/analyzer.py
@@ -10,20 +10,58 @@ class AnalyzerPipeline(OccupyPipeline):
 
 
 #### +=+=+=+ Analyzer Pipelines +=+=+=+ ####
-class SocialAction(AnalyzerPipeline):
+class SocialActionPipeline(AnalyzerPipeline):
 	
-	''' Pipeline for social action (upvote, downvote like, etc.). '''
+	''' Pipeline for social action (upvote, downvote, like, etc.). '''
 	
 	def run(self):
 		pass
 
 
-class ContentAction(AnalyzerPipeline):
+
+class ContentActionPipeline(AnalyzerPipeline):
 	
 	''' Pipeline for created content like comments, messages, pictures etc. '''
 	
 	def run(self):
 		pass
+
+
+
+class TopicCounter(ContentActionPipeline):
+
+	''' Pipeline used for counting topics '''
+
+	def run(self):
+		pass
+
+
+
+class CommentCounter(ContentActionPipeline):
+
+	''' Pipeline used for counting posted comments per topic. '''
+
+	def run(self):
+		pass
+
+
+
+class StarCounter(ContentActionPipeline):
+
+    ''' Pipeline used for counting stars on comments and topics. '''
+
+    def run(self):
+    	pass
+
+
+
+class VoteCounter(ContentActionPipeline):
+
+    ''' Pipeline for up/down votes on topics AND comments, per topic and comment. '''
+
+	def run(self):
+		pass
+
 
 
 
